6fd1b6219035c2c650d6a05b002e853e8d38869c
DEVU <DEVU@192.168.29.72>
2025-02-17 15:43:34 +0530

    COMMIT TEST2 INTO BRANCHX

---
+++ src/demogit/Gitdemo2.java
@@ -0,0 +1,13 @@
+package demogit;
+
+public class Gitdemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="hello";
+		String s2="hello";
+		System.out.println(s1==s2);
+		
+	}
+
+}
